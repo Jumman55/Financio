2d11e977c9bf9bcf23f29c0137b84207f3eecb96
Jumman55 <jumman.1948848@studenti.uniroma1.it>
2023-01-20 03:46:42 +0100

    Initial Commit

---
+++ Financio/FinancioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancioApp.swift
+//  Financio
+//
+//  Created by Jumman Hossen on 20/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FinancioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
