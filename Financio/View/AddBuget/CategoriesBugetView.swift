14f0b101f82adb734fb680f9ca989eb827b46d10
Jumman55 <jumman.1948848@studenti.uniroma1.it>
2023-02-09 18:25:26 +0100

    updated code from 9thFeb evening for adding expenses view

--- Financio/View/AddBuget/CategoriesBugetView.swift
+++ Financio/View/AddBuget/CategoriesBugetView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoriesBugetView: View {
     //MARK: - PROPERTIES
+    @State var amount: String = ""
     @State var cate = "Food"
     @State var note = ""
     @State var date = Date()
@@ -26,46 +27,74 @@ struct CategoriesBugetView: View {
                     .ignoresSafeArea()
                     
                 
-                VStack {
+                VStack(spacing:44) {
+                    Text("Add Expenses")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.top, 72)
                     
-                        Button{
-                            showPresentationMode1 = true
-                        } label: {
-                            AddExpensesButtonView()
-                        }
-                        .sheet(isPresented: $showPresentationMode1){
-                            AddDataCategoryButton(cate: $cate)
-                                .presentationDetents([.medium])
-                                .presentationDragIndicator(.visible)
-                        }
-                        Button{
-                            showPresentationMode2 = true
-                        } label: {
-                            AddExpensesButtonView(sfImagename: "note.text.badge.plus", titleName: "Note")
-                        }
-                        .sheet(isPresented: $showPresentationMode2){
-                            AddDataNoteButton(note: $note)
-                                .presentationDetents([.large])
-                                .presentationDragIndicator(.visible)
-                        }
-                      
-                        Button{
-                            showPresentationMode3 = true
-                        } label: {
-                            AddExpensesButtonView(sfImagename: "calendar", titleName: "Date")
-                        }
-                        .sheet(isPresented: $showPresentationMode3){
-                            AddDataDateButton(date: $date)
-                                .presentationDetents([.medium])
-                                .presentationDragIndicator(.visible)
-                        }
+                    ZStack {
+                        TextField("€ 0", text: $amount)
+                            .keyboardType(.numberPad)
+                            .font(.largeTitle)
+                            .fontWeight(.ultraLight)
+                             
                         
-                   
-                }
+                        
+                    }
+                    .frame(width: 300, height: 80)
+                    .padding(5)
+                    .background(.ultraThinMaterial, in:RoundedRectangle(cornerRadius: 20, style: .continuous))
+                    .overlay{
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(.linearGradient(colors:[.white.opacity(0.3), .black.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                            .blendMode(.overlay)
+                    }
+                    
+                    
+                    
+                    VStack(spacing: 10) {
+                        
+                            Button{
+                                showPresentationMode1 = true
+                            } label: {
+                                AddExpensesButtonView()
+                            }
+                            .sheet(isPresented: $showPresentationMode1){
+                                AddDataCategoryButton(cate: $cate)
+                                    .presentationDetents([.medium])
+                                    .presentationDragIndicator(.visible)
+                            }
+                            Button{
+                                showPresentationMode2 = true
+                            } label: {
+                                AddExpensesButtonView(sfImagename: "note.text.badge.plus", titleName: "Note")
+                            }
+                            .sheet(isPresented: $showPresentationMode2){
+                                AddDataNoteButton(note: $note)
+                                    .presentationDetents([.medium, .large])
+                                    .presentationDragIndicator(.visible)
+                            }
+                          
+                            Button{
+                                showPresentationMode3 = true
+                            } label: {
+                                AddExpensesButtonView(sfImagename: "calendar", titleName: "Date")
+                            }
+                            .sheet(isPresented: $showPresentationMode3){
+                                AddDataDateButton(date: $date)
+                                    .presentationDetents([.medium])
+                                    .presentationDragIndicator(.visible)
+                            }
+                            
+                       
+                    }//:VStackButtons
+                    .padding(.bottom, 317)
+                }//:Vstack
             
                 
             }//:ZStack
-            .navigationTitle("Details")
+           
         }//:Navi
         
     }
